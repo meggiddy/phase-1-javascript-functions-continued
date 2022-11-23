43f7fd562caffdf1f5c4a91eef9db1d35884b4d9
Maggie <muriithinyawira13@gmail.com>
2022-11-23 11:11:51 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(todo = "go to the office") {
+    return `This Monday, I will ${todo}.`;
+}
+function wrapAdjective(visualFlair = "*") {
+    return function (adjective = "special") {
+        return `You are ${visualFlair}${adjective}${visualFlair}!`
+    }
+}
